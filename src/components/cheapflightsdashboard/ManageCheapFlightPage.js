30e90099164882ffdea0682c544d6acd7541b015
Faiz Shaikh <faiz.shaikh@hotmail.com>
2017-10-17 17:06:32 +0100

    rte

--- src/components/cheapflightsdashboard/ManageCheapFlightPage.js
+++ src/components/cheapflightsdashboard/ManageCheapFlightPage.js
@@ -13,72 +13,93 @@ export class ManageCheapFlightPage extends React.Component {
 
     this.state = {
       cheapflight: Object.assign({}, props.cheapflight),
-      errors: {}
+      errors: {},
+      editorValue: RichTextEditor.createEmptyValue()
     };
 
     this.updateCheapFlightState = this.updateCheapFlightState.bind(this);
     this.saveCheapflight = this.saveCheapflight.bind(this);
+    this.updateEditorChanges = this.updateEditorChanges.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.cheapflight.id != nextProps.cheapflight.id) {
-      console.log(this.props);
+      console.log('this.props', this.props);
+      console.log('this.state', this.state);
+      console.log('nextprops', nextProps);
       // Necessary to populate form when existing cheapflight is loaded directly.
-      this.setState({ cheapflight: Object.assign({}, nextProps.cheapflight) });
+      this.setState({
+        cheapflight: Object.assign({}, nextProps.cheapflight)
+      });
+      let editorValue = this.state.editorValue;
+      console.log((editorValue.toString('html') !== nextProps.cheapflight.seoparagraph)); //true
+      if (editorValue.toString('html') !== nextProps.cheapflight.seoparagraph) {
+        editorValue = RichTextEditor.createValueFromString(nextProps.cheapflight.seoparagraph, 'html');
+        console.log(editorValue);
+        this.setState({editorValue: editorValue});
+      }
+      console.log({editorValue});
     }
   }
 
   updateCheapFlightState(event) {
     const field = event.target.name;
-    console.log(event.target.name, event.target.value)
     let cheapflight = Object.assign({}, this.state.cheapflight);
     cheapflight[field] = event.target.value;
-    if(event.target.name == 'destinationName'){
-      console.log("hi");
-      cheapflight.pageUrl = `/cheapflights-to-${cheapflight.destinationName.toLowerCase().split(' ').join('-')}.htm`
+    if (event.target.name == 'destinationName') {
+      cheapflight.pageUrl = `/cheapflights-to-${cheapflight
+        .destinationName
+        .toLowerCase()
+        .split(' ')
+        .join('-')}.htm`;
     }
-    if(event.target.name == 'seoparagraph'){
-      console.log("hi2");
-      event.target.value.toString('html')
-    }
-    return this.setState({ cheapflight: cheapflight });
+    return this.setState({cheapflight: cheapflight});
+  }
+
+  updateEditorChanges(editorValue) {
+    this.setState({editorValue});
+    console.log(editorValue.toString('html'));
+    let cheapflight = Object.assign({}, this.state.cheapflight);
+    cheapflight.seoparagraph = editorValue.toString('html');
+    console.log(cheapflight.seoparagraph);
+    return this.setState({cheapflight: cheapflight});
   }
 
   cheapFlightFormIsValid() {
     let formIsValid = true;
     let errors = {};
-
     if (this.state.cheapflight.user.length < 5) {
       errors.user = "Username must be at least 5 characters.";
       formIsValid = false;
     }
-
-    this.setState({ errors: errors });
+    this.setState({errors: errors});
     return formIsValid;
   }
 
   saveCheapflight(event) {
     event.preventDefault();
-
     if (!this.cheapFlightFormIsValid()) {
       return;
     }
-
-    this.setState({ saving: true });
-
-    this.props.actions
+    this.setState({saving: true});
+    this
+      .props
+      .actions
       .saveCheapflight(this.state.cheapflight)
       .then(() => this.redirect())
       .catch(error => {
         toastr.error(error);
-        this.setState({ saving: false });
+        this.setState({saving: false});
       });
   }
 
   redirect() {
-    this.setState({ saving: false });
+    this.setState({saving: false});
     toastr.success("CheapFlight saved");
-    this.context.router.push("/cheapflights-dashboard");
+    this
+      .context
+      .router
+      .push("/cheapflights-dashboard");
   }
 
   render() {
@@ -86,10 +107,12 @@ export class ManageCheapFlightPage extends React.Component {
       <CheapFlightForm
         allRegions={this.props.regions}
         onChange={this.updateCheapFlightState}
+        onEditorChange={this.updateEditorChanges}
         onSave={this.saveCheapflight}
         cheapflight={this.state.cheapflight}
         errors={this.state.errors}
         saving={this.state.saving}
+        editorValue={this.state.editorValue}
       />
     );
   }
@@ -122,7 +145,7 @@ function mapStateToProps(state, ownProps) {
     destinationName: "",
     heading1: "",
     heading2: "",
-    seoparagraph: RichTextEditor.createEmptyValue(),
+    seoparagraph: "",
     user: "",
     typeof: "",
     pageUrl: "",
@@ -133,6 +156,13 @@ function mapStateToProps(state, ownProps) {
 
   if (cheapFlightId && state.cheapflights.length > 0) {
     cheapflight = getCheapFlightById(state.cheapflights, cheapFlightId);
+    console.log(cheapflight);
+    let editorValue = RichTextEditor.createValueFromString(cheapflight.seoparagraph, 'html');
+    console.log(state);
+    editorValue = this.state.editorValue;
+    console.log('editorValue',state.editorValue);
+    editorValue = this.state.editorValue;
+
   }
 
   return {
